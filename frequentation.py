872db0511c05e9dd9d51b7e54a349fb9a8623a2b
LucasStagnette <103002404+LucasStagnette@users.noreply.github.com>
2023-12-15 16:00:28 +0100

    IC

--- frequentation.py
+++ frequentation.py
@@ -1,21 +1,15 @@
-# -*- coding:utf-8 -*-
 import json
+from formatage import formater_json
+
 def trie_frequentation_2022():
-    # ouverture et stockage dans une variable du fichier json
     with open("json_files/frequentation.json", "r") as fichier:
         data = json.load(fichier)
-    json_data = json.dumps(data, indent=2)  
-    data = json.loads(json_data)
     
-    max = -100000
-    nom = ""
-    for i in range(len(data)-1):
-        for gare in data:
-            nb = gare['total_voyageurs_2022']
-            if nb > max:
-                max = nb 
-                nom = gare['nom_gare']
+    data = sorted(data, key=lambda x: x['total_voyageurs_2022'], reverse=True)
 
+    with open("result_frequentation.json", 'w') as fichier:
+        json.dump(data, fichier, indent=2)
 
+    formater_json("result_frequentation.json")
 
 trie_frequentation_2022()
