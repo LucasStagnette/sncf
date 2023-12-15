73e194ceafdb90fb92a424ac4b91118ac708406b
LucasStagnette <103002404+LucasStagnette@users.noreply.github.com>
2023-12-15 15:16:25 +0100

    Update frequentation.py

--- frequentation.py
+++ frequentation.py
@@ -1,11 +1,21 @@
 # -*- coding:utf-8 -*-
-
 import json
-def trie_frequentation():
-    with open("j.json", "r") as fichier:
+def trie_frequentation_2022():
+    # ouverture et stockage dans une variable du fichier json
+    with open("json_files/frequentation.json", "r") as fichier:
         data = json.load(fichier)
-
     json_data = json.dumps(data, indent=2)  
-
     data = json.loads(json_data)
+    
+    max = -100000
+    nom = ""
+    for i in range(len(data)-1):
+        for gare in data:
+            nb = gare['total_voyageurs_2022']
+            if nb > max:
+                max = nb 
+                nom = gare['nom_gare']
 
+
+
+trie_frequentation_2022()
